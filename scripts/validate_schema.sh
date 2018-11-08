b47b22a1022496d90d62f2d1bec79d00b5519e2c
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-11-08 11:09:21 +0100

    Entity Temporal

--- scripts/validate_schema.sh
+++ scripts/validate_schema.sh
@@ -3,3 +3,5 @@
 ajv compile -s $SCHEMAS/Entity.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
 
 ajv compile -s $SCHEMAS/Subscription.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
+
+ajv compile -s $SCHEMAS/EntityTemporal.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
