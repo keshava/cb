8885d7af995b6430bf264d723114b1726e283612
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-11-09 10:14:51 +0100

    Validate Schema

--- scripts/validate_schema.sh
+++ scripts/validate_schema.sh
@@ -5,3 +5,5 @@ ajv compile -s $SCHEMAS/Entity.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry
 ajv compile -s $SCHEMAS/Subscription.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
 
 ajv compile -s $SCHEMAS/EntityTemporal.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
+
+ajv compile -s $SCHEMAS/ContextSourceRegistration.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
