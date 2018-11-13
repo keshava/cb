4852d8007234aa3b46fe54c8433e031e40a4d537
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-11-13 13:18:50 +0100

    Refining more and more. Now using RegExp for Names

--- scripts/validate_schema.sh
+++ scripts/validate_schema.sh
@@ -14,6 +14,8 @@ ajv compile -s $SCHEMAS/ContextSourceNotification.json -r $SCHEMAS/common.json -
 
 ajv compile -s $SCHEMAS/EntityList.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
 
+ajv compile -s $SCHEMAS/EntityTemporalList.json  -r $SCHEMAS/EntityTemporal.json -r $SCHEMAS/Entity.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
+
 ajv compile -s $SCHEMAS/BatchOperationResult.json -r $SCHEMAS/common.json
 
 ajv compile -s $SCHEMAS/UpdateResult.json
