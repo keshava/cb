3fc37d624196e71c3936c2d75816729619bdbd77
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-11-13 10:53:48 +0100

    Batch Operation Result

--- scripts/validate_schema.sh
+++ scripts/validate_schema.sh
@@ -14,3 +14,4 @@ ajv compile -s $SCHEMAS/ContextSourceNotification.json -r $SCHEMAS/common.json -
 
 ajv compile -s $SCHEMAS/EntityList.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
 
+ajv compile -s $SCHEMAS/BatchOperationResult.json -r $SCHEMAS/common.json 
