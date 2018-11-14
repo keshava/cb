869c752f862fbf4478a53a0a47803c5f47b9e04a
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-11-14 18:20:18 +0100

    Validate Examples

--- scripts/validate_examples.sh
+++ scripts/validate_examples.sh
@@ -6,27 +6,17 @@ ajv validate -s $SCHEMAS/Entity.json -d $EXAMPLES/Vehicle_C2.2.json -r $SCHEMAS/
 
 ajv validate -s $SCHEMAS/Entity.json -d $EXAMPLES/OffStreetParking_C2.3.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
 
-ajv validate -s $SCHEMAS/Entity_keyValues.json -d -d $EXAMPLES/Vehicle_keyValues_C2.2.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json 
+ajv validate -s $SCHEMAS/Entity_keyValues.json -d -d $EXAMPLES/Vehicle_keyValues_C2.2.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
 
-ajv compile -s $SCHEMAS/subscriptions/Subscription.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
+ajv validate -s $SCHEMAS/Entity_keyValues.json -d -d $EXAMPLES/OffStreetParking_keyValues_C2.3.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
 
-ajv compile -s $SCHEMAS/temporal/EntityTemporal.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
+ajv validate -s $SCHEMAS/Entity.json -d $EXAMPLES/Vehicle_MultiAttribute_C2.2.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
 
-ajv compile -s $SCHEMAS/registrations/ContextSourceRegistration.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
+ajv validate -s $SCHEMAS/subscriptions/Subscription.json -d $EXAMPLES/Subscription-example_C.4.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
 
-ajv compile -s $SCHEMAS/subscriptions/Notification.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
+ajv validate -s $SCHEMAS/registrations/ContextSourceRegistration.json -d $EXAMPLES/ContextSourceRegistration-example_C.3.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
 
-ajv compile -s $SCHEMAS/registrations/ContextSourceNotification.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/registrations/ContextSourceRegistration.json
+ajv validate -s $SCHEMAS/temporal/EntityTemporal.json -d $EXAMPLES/EntityTemporal-example_C5.5.3.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
 
-ajv compile -s $SCHEMAS/EntityList.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
-
-ajv compile -s $SCHEMAS/temporal/EntityTemporalList.json  -r $SCHEMAS/temporal/EntityTemporal.json -r $SCHEMAS/Entity.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json
-
-ajv compile -s $SCHEMAS/BatchOperationResult.json -r $SCHEMAS/common.json
-
-ajv compile -s $SCHEMAS/UpdateResult.json
-
-
-
-ajv compile -s $SCHEMAS/temporal/Entity_temporalValues.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
+ajv validate -s $SCHEMAS/temporal/Entity_temporalValues.json -d  $EXAMPLES/Entity_temporalValues_C.5.6.3.json -r $SCHEMAS/common.json -r $SCHEMAS/geometry-schema.json -r $SCHEMAS/Entity.json
 
