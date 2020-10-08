8b5fde29cff1c592401539f43a46eae3e43275ad
André Costa <acosta@ubiwhere.com>
2020-10-08 11:44:48 +0200

    Merge of private repo to Master

--- scripts/validate_online.sh
+++ scripts/validate_online.sh
@@ -3,22 +3,10 @@
 # Requires install swagger-cli: npm install -g swagger-cli
 #
 
-
-# Store current directory
-current_dir=${PWD}  
-
-# Store script directory
-script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-
-# Move cd to the script directory (the script must be called from the 'script' directory)
-cd $script_directory
-
 # Merge the swagger definition into an unified and de-referenced file (use this file will avoid reference resolution problems during the validation)
-swagger-cli bundle -r ../spec/ngsild_swagger.json -o combined_spec/ngsild_swagger_combined_dereferenced.json
-
+swagger-cli bundle --dereference ./spec/updated/ngsi-ld-spec-open-api.json -o ./bundle/ngsild_swagger_combined_dereferenced.json &&
+# modified version to allow circular ref
+# ~/work/swagger-cli/bin/swagger-cli.js bundle --dereference="ignore" ./spec/updated/ngsi-ld-spec-open-api.json -o ./bundle/ngsild_swagger_combined_dereferenced.json &&
+echo '--> Sending to validator.swagger.io' &&
 # Validate the swagger definition against the official online swagger validation service
-curl -X POST -d @combined_spec/ngsild_swagger_combined_dereferenced.json -H 'Content-Type:application/json' http://online.swagger.io/validator/debug
-
-# Move cd to the directory from where the script was called
-cd $current_dir
+curl -X POST -d @bundle/ngsild_swagger_combined_dereferenced.json -H 'Content-Type:application/json' https://validator.swagger.io/validator/debug
